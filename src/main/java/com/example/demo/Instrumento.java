38502749f7dbf04f537ac1047f6f957a0104076b
SamSeijuro <samseijuro@gmail.com>
2023-07-27 22:29:11 -0500

    commit

---
+++ src/main/java/com/example/demo/Instrumento.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "instrumento")
+public class Instrumento {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private String nombre;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_categoria")
+    private Instrumento instrumento;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Instrumento getInstrumento() {
+        return instrumento;
+    }
+
+    public void setInstrumento(Instrumento instrumento) {
+        this.instrumento = instrumento;
+    }
+
+    
+    
+}
